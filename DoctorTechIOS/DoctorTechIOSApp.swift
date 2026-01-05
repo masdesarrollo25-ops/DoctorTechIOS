0fe4a762645e47af08091c780ac26d9658749bd5
Mas Desarrollo <masdesarrollo25@gmail.com>
2026-01-05 07:38:53 +0100

    Initial Commit

---
+++ DoctorTechIOS/DoctorTechIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorTechIOSApp.swift
+//  DoctorTechIOS
+//
+//  Created by MAS DESAROLLO on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DoctorTechIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
